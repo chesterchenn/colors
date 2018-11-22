434c26499eea96ed54007b23b623d597d19c0ff8
chesterchenn <chesterchenn@gmail.com>
2018-11-22 14:06:13 +0800

    config color

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.end('Hello World'));
+
+app.route('/colors')
+  .get((req, res) => {
+    res.send(fs.readFileSync('../config/colors.json', 'utf8'))
+    res.end();
+  })
 
 app.listen(port);
