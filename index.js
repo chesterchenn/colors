bb4d18c898bf183eafd580fcfa3ed50bd1f30a62
chesterchenn <chesterchenn@gmail.com>
2019-03-18 14:23:00 +0800

    remove useless things

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const port = 3030;
 
@@ -8,22 +7,5 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-const db = fs.readFileSync('../mock/db.json', 'utf8');
-const data = JSON.parse(db);
-
-app.route('/api/db')
-  .get((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-    res.end();
-  });
-
-app.route('/api/db/colors')
-  .get((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data.colors);
-    res.end();
-  });
 
 app.listen(port);
