9cc42369205f199b2ee54d4536d842f813a218e3
chesterchenn <chesterchenn@gmail.com>
2019-09-02 16:33:39 +0800

    Return when token is invalid

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,14 +7,13 @@ const auth = (req, res, next) => {
     return next();
   }
   let token = req.headers["authorization"];
-  try {
-    // remove 'Bearer ' from string;
-    token = token.slice(7);
-  } catch (err) {
-    console.log(err);
+  if (!token) {
+    console.log('未登录，无法检测到token');
     return res.status(401).send("未登录");
   }
   try {
+    // remove 'Bearer ' from string;
+    token = token.slice(7);
     const decode = jwt.verify(token, config.privateKey);
     req.user = decode;
     next();
